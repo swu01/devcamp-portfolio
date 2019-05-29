6cd47d2bf025eea5ab7f82b607759986a45dfaa1
swu01 <swucode@gmail.com>
2019-05-29 16:27:18 -0400

    Created seed file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}"
+		blog: "Enim deserunt elit adipisicing eiusmod adipisicing in non dolor ut eu sunt culpa in sed duis. 
+		Mollit qui officia commodo aute et sit consequat minim. Magna in sit cupidatat dolor sed reprehenderit culpa ut laborum aute. Commodo ex exercitation consequat duis consectetur officia qui occaecat fugiat exercitation. Lorem ipsum elit sed fugiat aliqua amet laboris adipisicing nostrud incididunt.
+		Pariatur incididunt deserunt non laboris cupidatat exercitation nisi excepteur qui dolore. Quis tempor veniam esse veniam proident proident incididunt quis irure mollit dolor laborum adipisicing officia minim."
+	)
+end 
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end 
+
+puts "5 skills created"
+
+9.times do |porfolio_item|
+	Portfolio.create!(
+	title: "Portfolio title #{portfolio_title}",
+	subtitle: "My great service",
+	body: "Dolore exercitation eiusmod in sit anim quis dolore voluptate adipisicing ex et quis incididunt aute sint nisi. Sint tempor laboris elit elit laboris exercitation cupidatat proident sed eiusmod magna. In eu nulla dolore reprehenderit sit ut tempor deserunt fugiat est dolor laboris mollit. Nostrud dolor culpa mollit ut ea adipisicing nostrud elit dolore eu exercitation in sed esse id.",
+	main_image: "http://placehold.it/600x400",
+	thumb_image: "http://placehold.it/350x200"
+	)
+end 
